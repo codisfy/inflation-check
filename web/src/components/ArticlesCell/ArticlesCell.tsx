2711e5e014ef64bdc7ed804a526f4a3f4f5e67b6
nikhil <codisfy@gmail.com>
2024-02-20 20:39:24 -0500

    End of chapt 2

--- web/src/components/ArticlesCell/ArticlesCell.tsx
+++ web/src/components/ArticlesCell/ArticlesCell.tsx
@@ -5,6 +5,7 @@ import type {
   CellFailureProps,
   TypedDocumentNode,
 } from '@redwoodjs/web'
+import Article from 'src/components/Article'
 
 export const QUERY: TypedDocumentNode<
   ArticlesQuery,
@@ -32,13 +33,7 @@ export const Success = ({ articles }: CellSuccessProps<ArticlesQuery>) => {
   return (
     <>
       {articles.map((article) => (
-        <article key={article.id}>
-          <header>
-            <h2>{article.title}</h2>
-          </header>
-          <p>{article.body}</p>
-          <div>Posted at: {article.createdAt}</div>
-        </article>
+         <Article key={article.id} article={article} />
       ))}
     </>
   )
