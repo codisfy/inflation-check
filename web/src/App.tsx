035dd8dbc42037b2db15387fcb2838ce2ade5b2e
nikhil <codisfy@gmail.com>
2024-02-23 18:16:32 -0500

    add preline and setup tailwind

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,7 +3,7 @@ import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
-
+import "preline/preline";
 import { AuthProvider, useAuth } from './auth'
 
 import './scaffold.css'
